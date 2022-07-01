80327ab83ecad670d7048d2a5eda49bbce3aeb85
Renato <renatoft89@gmail.com>
2022-07-01 18:19:52 -0300

    feat: add add controller getAllTasks

---
+++ back/src/controllers/taskController.js
@@ -0,0 +1,14 @@
+const getAllService = require('../services/taskService');
+
+const getAllTasks = async (req, res, next) => {
+  try {
+    const result =  await getAllService();
+
+    return res.status(200).json(result)
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+}
+
+module.export = getAllTasks;
