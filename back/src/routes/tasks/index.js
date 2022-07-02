faecfd2c82b9e33bf7118927440fd14afb8e706a
Renato <renatoft89@gmail.com>
2022-07-02 14:25:26 -0300

    feat: create route post task

--- back/src/routes/tasks/index.js
+++ back/src/routes/tasks/index.js
@@ -4,5 +4,6 @@ const taskController = require('../../controllers/taskController');
 const tasksRouter = express.Router();
 
 tasksRouter.get('/', taskController.getAllTasks);
+tasksRouter.post('/', taskController.createTask);
 
 module.exports = tasksRouter
