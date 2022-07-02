f7e8c25a0dc8ef36d17d01f10e84640805d7eb65
Renato <renatoft89@gmail.com>
2022-07-02 18:44:40 -0300

    feat: config axios

--- front/src/components/Tasks.jsx
+++ front/src/components/Tasks.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { useEffect } from 'react';
+import api from '../services/index'
 import '../styles/Tasks.css'
 
 function Tasks() {
+  useEffect(() => {
+    api
+      .get("/tasks").then((response) => (console.log(response)))
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
   return (
     <div className='tasks'>
       <p>Tasks</p>
