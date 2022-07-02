38519cf83831bea65a6a93e80e111972f643a51a
Renato <renatoft89@gmail.com>
2022-07-02 18:17:41 -0300

    feat: config css components

---
+++ front/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css'
+
+function Header() {
+  return (
+    <div className='header'>
+      <p>Este é o Header...</p>
+    </div>
+  );
+}
+
+export default Header;
