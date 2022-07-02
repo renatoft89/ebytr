bcb7624e7ca33f64da0020d7a1f8021648d32432
Renato <renatoft89@gmail.com>
2022-07-02 16:29:42 -0300

    fix: rm back to branch front-end

--- back/src/server.js
+++
@@ -1,6 +0,0 @@
-require('dotenv').config();
-const app = require('./api');
-
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => console.log('Online Port: ', PORT, `http://localhost:${PORT}/`));
