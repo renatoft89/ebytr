51588fef544ca955eaf160ac7d298a418307d091
renatoft89 <renatoft89@gmail.com>
2023-02-03 18:58:00 -0300

    fix: deploy env database

--- back/src/models/connection.js
+++ back/src/models/connection.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 
 const connection = mysql.createPool({
-  host: process.env.MYSQL_HOST || 'containers-us-west-136.railway.app',
-  user: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'bttQerUf4C4jVtoDvaMg',
-  database: process.env.MYSQL_DATABASE || 'railway',
-  port: process.env.MYSQL_PORT || '6155',
+  host: process.env.MYSQLHOST || 'containers-us-west-136.railway.app',
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'bttQerUf4C4jVtoDvaMg',
+  database: process.env.MYSQLDATABASE || 'railway',
+  port: process.env.MYSQLPORT || '6155',
 });
 
 module.exports = connection;
