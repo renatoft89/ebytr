830dfcaa13ca7dc179aa08731c9e6eaffbc8d589
renatoft89 <renatoft89@gmail.com>
2023-02-09 15:24:22 -0300

    fix: variaveis de ambiente mysql

--- back/src/models/connection.js
+++ back/src/models/connection.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config(); // não se esqueça de configurar suas variáveis de ambiente aqui na configuração
 
 const connection = mysql.createPool({
-  host: process.env.MYSQLHOST || 'containers-us-west-136.railway.app',
-  user: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'bttQerUf4C4jVtoDvaMg',
-  database: process.env.MYSQLDATABASE || 'railway',
-  port: process.env.MYSQLPORT || '6155',
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
 });
 
 module.exports = connection;
